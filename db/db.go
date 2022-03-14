1badca78682ca408691286e92feed781278db55a
Giulio <giuliohome@gmail.com>
2022-03-14 11:09:46 -0400

    local postgresql

---
+++ db/db.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"os"
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+)
+
+const (
+	port     = 5432
+	user     = "postgres"
+	dbname   = "postgres"
+)
+
+func main() {
+	// connection string
+	host := os.Getenv("DBHOST")
+	password :=  os.Getenv("DBPSW")
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	// open database
+	db, err := sql.Open("postgres", psqlconn)
+	CheckError(err)
+
+	// close database
+	defer db.Close()
+
+	// check db
+	err = db.Ping()
+	CheckError(err)
+
+	fmt.Println("Connected!")
+
+	// Delete
+	deleteStmt := "delete from distributors where did in ($1,$2)"
+	_, err = db.Exec(deleteStmt, 1, 2)
+	CheckError(err)
+
+	// dynamic
+	insertDynStmt := `insert into distributors (name, did) values($1, $2)`
+	_, err = db.Exec(insertDynStmt, "John", 1)
+	CheckError(err)
+	_, err = db.Exec(insertDynStmt, "Jane", 2)
+	CheckError(err)
+
+	// update
+	updateStmt := `update distributors set name=$1 where did=$2`
+	_, err = db.Exec(updateStmt, "Mary", 2)
+	CheckError(err)
+
+	rows, err := db.Query(`SELECT name, did FROM distributors`)
+	CheckError(err)
+
+	defer rows.Close()
+	for rows.Next() {
+		var name string
+		var did int
+
+		err = rows.Scan(&name, &did)
+		CheckError(err)
+
+		fmt.Println(name, did)
+	}
+
+	CheckError(err)
+}
+
+func CheckError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
