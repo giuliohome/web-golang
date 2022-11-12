6a2941a3667439c6d2b4f65641fbe16716112e19
Giulio <giuliohome@gmail.com>
2022-11-12 04:32:44 +0100

    web app to db

--- web.go
+++ web.go
@@ -12,8 +12,69 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"regexp"
+	"database/sql"
+        _ "github.com/lib/pq"
+
 )
+const (
+	port = 5432
+)
+
+
+func connDB() (*sql.DB, error) {
+	// connection string
+	host := os.Getenv("DBHOST")
+	password :=  os.Getenv("DBPSW")
+	dbname := os.Getenv("POSTGRES_DB")
+	user := os.Getenv("POSTGRES_USER")
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	fmt.Println("psqlconn: %s", psqlconn)
+
+	// open database
+	db, err := sql.Open("postgres", psqlconn)	
+	
+	return db, err
+}
+
+func readDB(key string) ([]byte, error) {
+	db, err := connDB()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+	
+	rows, err := db.Query("SELECT name FROM mypages WHERE key = $1", key)
+	if err != nil {
+		return nil, err
+	}
+
+	nxt := rows.Next()
+	if !nxt {
+		return nil, nil
+	}
+	var name string
+	err = rows.Scan(&name)
+	return []byte(name), err
+}
+func writeDB(key string, name string) error {
+	db, err := connDB()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	deleteDynStmt := `delete from mypages where key = $1`
+	_, err = db.Exec(deleteDynStmt, key)
+	if err != nil {
+		return err
+	}
+
+	insertDynStmt := `insert into mypages (key,name) values($1, $2)`
+	_, err = db.Exec(insertDynStmt, key, name)
+	return err
+}
 
 type Page struct {
 	Title string
@@ -21,13 +82,22 @@ type Page struct {
 }
 
 func (p *Page) save() error {
+	if strings.HasPrefix(p.Title,"db") {
+		return writeDB(p.Title, string(p.Body))
+	}
 	filename := p.Title + ".txt"
 	return os.WriteFile(filename, p.Body, 0600)
 }
 
 func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := os.ReadFile(filename)
+	var body []byte
+	var err error 
+	if strings.HasPrefix(title,"db") {
+		body, err = readDB(title)
+	} else {
+		filename := title + ".txt"
+		body, err = os.ReadFile(filename)
+	}
 	if err != nil {
 		return nil, err
 	}
